764dd29837db869ea3b3a3e3b238a216c200789d
alifzwan <alifzakwan529@gmail.com>
2023-11-17 11:06:22 +0800

    change "Retailing" => Retail, Sold => sold. Will be changing as it initial form on .sol soon

--- client/src/components/showcase/admin/Admin.jsx
+++ client/src/components/showcase/admin/Admin.jsx
@@ -156,7 +156,7 @@ const Admin = () => {
     const adminRetail = async (event) => {
         event.preventDefault();
         try {
-            var receipt = await SupplyChain.methods.Retailing(ItemID).send({ from: currentaccount });
+            var receipt = await SupplyChain.methods.Retail(ItemID).send({ from: currentaccount });
             if (receipt) {
                 loadBlockchaindata();
             }
@@ -170,7 +170,7 @@ const Admin = () => {
     const adminSold = async (event) => {
         event.preventDefault();
         try {
-            var receipt = await SupplyChain.methods.Sold(ItemID).send({ from: currentaccount });
+            var receipt = await SupplyChain.methods.sold(ItemID).send({ from: currentaccount });
             if (receipt) {
                 loadBlockchaindata();
             }
