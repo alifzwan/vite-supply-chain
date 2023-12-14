f20e3659e73783678f7e20d2f7925a346d51bb53
alifzwan <alifzakwan529@gmail.com>
2023-12-14 08:06:27 +0800

    Adding Authentication Section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,11 +4,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "./components/home/Home"
 import Project from "./components/showcase/project/Project";
+import ProjectHero from "./components/showcase/project/projectHero/ProjectHero";
 import Register from "./components/showcase/register/Register";
 import Order from "./components/showcase/order/Order";
 import Admin from "./components/showcase/admin/Admin";
 import Track from "./components/showcase/track/Track";
 import Info from "./components/showcase/info/Info";
+import Login from "./components/showcase/login/Login";
+
 
 const App = () => {
   return (
@@ -17,6 +20,8 @@ const App = () => {
     <Routes>
           <Route path = '/' exact element={<Home />}/>
           <Route path = '/project' exact  element={<Project />} />
+          <Route path = '/projecthero' exact  element={<ProjectHero />} />
+          <Route path = '/login' exact  element={<Login />} />
           <Route path = '/register' exact  element={<Register />} />
           <Route path = '/order' exact  element={<Order />} />
           <Route path = '/admin' exact  element={<Admin />} />
--- client/src/components/showcase/login/Login.jsx
+++ client/src/components/showcase/login/Login.jsx
@@ -1,65 +1,105 @@
-import React, { useContext } from 'react'
-import "./login.scss"
-import { Link, Navigate } from 'react-router-dom'
-import MetamaskPNG from "/metamask.png"
-import AppPNG from "/app.png"
-import { AppContext } from '../../utils/AppContext'
 
+import './login.scss';
+import { motion } from "framer-motion";
+import { useNavigate } from "react-router-dom"
+import React, { useState, useEffect } from 'react'
+import Web3 from "web3";
+
+const itemVariants = {
+    open: {
+        y: 0,
+        opacity: 1,
+    },
+    closed: {
+        y: 50,
+        opacity: 0,
+    },
+};
 
 const Login = () => {
+    const navigate = useNavigate()
 
-    const { web3, accounts } = useContext(AppContext)
+    const redirect_to_home = () => {
+        navigate('/');
+    };
+    const redirect_to_project = () => {
+      navigate('/project')
+    }
+
+    const [web3, setWeb3] = useState(null);
+    const [accounts, setAccounts] = useState([]);
+
+    useEffect(() => {
+        const loadWeb3 = async () => {
+        if (window.web3) {
+            window.web3 = new Web3(window.web3.currentProvider);
+            setWeb3(window.web3);
+        } else {
+            window.alert(
+            "Non-Ethereum browser detected. You should consider trying MetaMask!"
+            );
+        }
+        };
+
+        loadWeb3();
+        
+    }, []);
+
 
     const handleConnect = async (e) => {
-        e.preventDefault()
+        e.preventDefault();
+        try {
         await window.ethereum.request({
             method: "wallet_requestPermissions",
-            params: [{eth_accounts: {}}]
+            params: [{ eth_accounts: {} }],
         });
         const address = await window.ethereum.request({
             method: "eth_requestAccounts",
-            params: [{  }]
-        })
-        let ad = web3.utils.toChecksumAddress(address[0])
-        const networkId = await web3.eth.net.getId();
-        localStorage.setItem('cacheKey', ad)
-        localStorage.setItem('cacheNID', networkId)
-        window.location.href = "/all"
-    }
+            params: [{}],
+        });
+        let ad = window.web3.utils.toChecksumAddress(address[0]);
+        setAccounts([ad]);
+        } catch (error) {
+        console.error("Error connecting:", error);
+        }
+    };
 
-    if(accounts.length > 0){
-        return <Navigate to="/all" />
+    if (accounts.length > 0) {
+        return redirect_to_project();
     }
 
     return (
-        <div className="create-container">
-            <Link to="/" className="backto-home">Back to home</Link>
-            <div className="split-container">
-
-                <div className="form-container auth">
-                    <div className="form">
-                        <h2 className="heading">Log In</h2>
-
-                        <div className="login-dg">
-                            <img width="60" height="60" src={MetamaskPNG} alt="" />
-                            <p>- - - - - - -</p>
-                            <img width="60" height="60" src={AppPNG} alt="" />
-                        </div>
-
-                        <p className="login-message">Login with Metamask</p>
-
-                        <div className="flex-container">
-                            <button onClick={handleConnect}>Connect Metamask</button>
-                            <a href="https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn">
-                                Add Metamask To Chrome
-                            </a>
-                        </div>
+        <div className="login-main-container">
+            <div className="login-section">
+                <div className="login">
+                    <h2 className="login-title">Login</h2>
+                    <div className="login-icon">
+                        <img width="60" height="60" src={"/metamask.png"} alt="" />
+                        <p>- - - - - - -</p>
+                        <img width="60" height="60" src={"/app.png"} alt="" />
+                    </div>
+                    <p className="login-message">Login with Metamask</p>
+                    <div className="flex-login-container">
+                        <button onClick={handleConnect}>Connect Metamask</button>
+                        <a href="https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn">
+                        Add Metamask To Chrome
+                    </a>
                     </div>
                 </div>
-
             </div>
+            
+            <motion.div variants={itemVariants} className="back-button">
+                        <motion.button
+                            variants={itemVariants}
+                            whileHover={{ scale: 1.1 }}
+                            whileTap={{ scale: 0.95 }}
+                            onClick={redirect_to_home}
+                        >
+                            Back to Project Overview
+                        </motion.button>
+            </motion.div>
         </div>
-    )
+    );
 }
 
-export default Login
+export default Login;
