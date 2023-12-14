9e1d18bb3e850056ecf74bbb96be4e765e669a00
alifzwan <alifzakwan529@gmail.com>
2023-12-14 08:06:39 +0800

    updating import

--- client/src/components/home/about/aboutNavbar/AboutNavBar.jsx
+++ client/src/components/home/about/aboutNavbar/AboutNavBar.jsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 
 import EmailRoundedIcon from "@mui/icons-material/EmailRounded";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import GithubIcon from "@mui/icons-material/Github";
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 const variants ={
   initial:{ 
@@ -49,7 +49,7 @@ const AboutNavbar = () => {
 
         <div className="social">
           <a href="https://github.com/alifzwan" target="_blank" rel="noopener noreferrer">
-            <GithubIcon style={{fontSize: 30}}></GithubIcon>
+            <GitHubIcon style={{fontSize: 30}}></GitHubIcon>
           </a>
           <a href="https://open.spotify.com/user/alifzwan?si=b4ddb3a5803e4954" target="_blank" rel="noopener noreferrer">
             <img src="/spotify.svg" alt="" />
