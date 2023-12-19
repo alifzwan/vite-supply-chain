d92bbf2e2cdf9d37c073dad77f2c1a26f3f4853d
alifzwan <alifzakwan529@gmail.com>
2023-12-20 00:50:26 +0800

    Added Blockchain and HalalVerify Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,9 +4,11 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "./components/home/Home"
 import Project from "./components/showcase/project/Project";
+import Blockchain from "./components/showcase/blockchain/Blockchain";
 import ProjectHero from "./components/showcase/project/projectHero/ProjectHero";
 import Register from "./components/showcase/register/Register";
 import Order from "./components/showcase/order/Order";
+import HalalVerify from "./components/showcase/halalverify/HalalVerify";
 import Admin from "./components/showcase/admin/Admin";
 import Track from "./components/showcase/track/Track";
 import Info from "./components/showcase/info/Info";
@@ -20,10 +22,12 @@ const App = () => {
     <Routes>
           <Route path = '/' exact element={<Home />}/>
           <Route path = '/project' exact  element={<Project />} />
+          <Route path = '/blockchain' exact  element={<Blockchain />} />
           <Route path = '/projecthero' exact  element={<ProjectHero />} />
           <Route path = '/login' exact  element={<Login />} />
           <Route path = '/register' exact  element={<Register />} />
           <Route path = '/order' exact  element={<Order />} />
+          <Route path = '/verify' exact  element={<HalalVerify />} />
           <Route path = '/admin' exact  element={<Admin />} />
           <Route path = '/track' exact  element={<Track />} />
           <Route path = '/info' exact  element={<Info />} />
