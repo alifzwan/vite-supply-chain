bb411bb914ee3e9d264d21b8c2a38ca88ca263bc
alifzwan <alifzakwan529@gmail.com>
2023-12-21 18:33:29 +0800

    Added Frameworks links

--- client/src/components/home/about/aboutBotbar/AboutBotBar.jsx
+++ client/src/components/home/about/aboutBotbar/AboutBotBar.jsx
@@ -1,5 +1,6 @@
 import Sidebar from "../../sidebar/Sidebar";
 import "./aboutbotbar.scss";
+import HomeLinksFramework from "../../homepage/homelinks/HomeLinkFramework";
 import { motion } from "framer-motion";
 import { useNavigate } from "react-router-dom"
 
@@ -32,7 +33,7 @@ const AboutBotBar = () => {
     <div className="navbar">
       <Sidebar/>
       <div className="redirect-to-project-button">
-          <motion.a>
+          <motion.a className="project-work-button">
                 <motion.button
                     variants={itemVariants}
                     whileHover={{ scale: 1.1 }}
@@ -40,6 +41,16 @@ const AboutBotBar = () => {
                     onClick={redirect_to_login}
                 >
                    See How My Project Work
+                </motion.button>
+          </motion.a>
+          <motion.a className="framework-work-button">
+                <motion.button
+                    variants={itemVariants}
+                    whileHover={{ scale: 1.1 }}
+                    whileTap={{ scale: 0.95 }}
+                    
+                >
+                   <HomeLinksFramework />
                 </motion.button>
           </motion.a>
         
