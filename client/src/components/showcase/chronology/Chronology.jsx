eeb968a2c5715d89bebfc9c0d2bafe7623b3f72a
alifzwan <alifzakwan529@gmail.com>
2024-01-08 23:36:00 +0800

    Update Description

--- client/src/components/showcase/chronology/Chronology.jsx
+++ client/src/components/showcase/chronology/Chronology.jsx
@@ -33,11 +33,11 @@ const Chronology = () => {
             <div className="flow-section-title">Tasks Description</div>
             <div className="flow-content">
                 <ul className="tasklist">
-                    <a href="/register"><li>Punch In: <p>NGO register every Stakeholders</p></li></a>
-                    <a href="/order"><li>Registration: <p>Register the Product</p></li></a>
-                    <a href="/admin"><li>Administration: <p>NGO will change the PHASE of the product (Ex: from Poultry to Slaughterhouse)</p></li></a>
-                    <a href="/slaughterhouse"><li>Slaughter Verification: <p>The Product is being Slaughter (Tempat Penyembelihan)</p></li></a>
-                    <a href="/verify"><li>Halal Verification: <p>The Product is being verify the Halal Status</p></li></a>
+                    <a href="/register"><li>Punch In: <p>NGO/GLC register every Stakeholders</p></li></a>
+                    <a href="/order"><li>Registration: <p>NGO/GLC Register the Product</p></li></a>
+                    <a href="/admin"><li>Administration: <p>NGO/GLC will change the PHASE of the product (E.g. from Poultry to Slaughterhouse)</p></li></a>
+                    <a href="/slaughterhouse"><li>Slaughter Verification: <p>Slaughterhouse conduct slaughter verification</p></li></a>
+                    <a href="/verify"><li>Halal Verification: <p>Halal Verifier (JAKIM) verify the Halal Status</p></li></a>
                     <a href="/track"><li>Track Product: <p>Tracking the Product PHASE</p></li></a>
                     <a href="/info"><li>Product Info: <p>Retrieve the Product information</p></li></a>
                 </ul>
