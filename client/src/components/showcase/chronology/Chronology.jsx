14e9b8b0666c78b2de05fa11b0a40d71828c4a1e
alifzwan <alifzakwan529@gmail.com>
2024-01-16 06:25:02 +0800

    added mardi task

--- client/src/components/showcase/chronology/Chronology.jsx
+++ client/src/components/showcase/chronology/Chronology.jsx
@@ -35,7 +35,8 @@ const Chronology = () => {
                 <ul className="tasklist">
                     <a href="/register"><li>Punch In: <p>NGO/GLC register every Stakeholders</p></li></a>
                     <a href="/order"><li>Registration: <p>NGO/GLC Register the Product</p></li></a>
-                    <a href="/admin"><li>Administration: <p>NGO/GLC will change the PHASE of the product (E.g. from Poultry to Slaughterhouse)</p></li></a>
+                    <a href="/admin"><li>Administration: <p>NGO/GLC will change the PHASE of the product (E.g. from Poultry to Mardi)</p></li></a>
+                    <a href="/mardi"><li>Mardi Verification: <p>Mardi will conduct a quality check on Poultry</p></li></a>
                     <a href="/slaughterhouse"><li>Slaughter Verification: <p>Slaughterhouse conduct slaughter verification</p></li></a>
                     <a href="/verify"><li>Halal Verification: <p>Halal Verifier (JAKIM) verify the Halal Status</p></li></a>
                     <a href="/track"><li>Track Product: <p>Tracking the Product PHASE</p></li></a>
